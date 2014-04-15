61b1b03b804dbbfe53e520004e2b9a93adac14de
Joe Betz <jbetz@linkedin.com>
2014-04-15 14:01:14 -0700

    update homebrew formula

--- Library/Formula/restli.rb
+++ Library/Formula/restli.rb
@@ -3,7 +3,7 @@ require "formula"
 class Restli < Formula
   homepage "http://rest.li/"
   url "http://rest.li/releases/restli-tool/0.0.1/restli-0.0.1.tar.gz"
-  sha1 "dceab18f7cbc2aba0f4738ebd9cfa2501846988a"
+  sha1 "1b11f3012d112f63250f5721dd82a501b1e42014"
   
   depends_on "giter8"
 
